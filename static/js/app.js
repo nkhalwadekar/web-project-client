029be7ff33d82164f7dd8c0d1700e35b19839c81
Neha Khalwadekar <nehakhalwadekar@Nehas-MacBook-Pro.local>
2016-11-13 18:44:43 -0500

    step 5 changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   console.log("App Started");
 
-  $("#echo_button").click(function() {
+  $("#message_button").click(function() {
 
-    var echoString = $("#echo_input").val();
+    var message = $("#message_input").val();
 
     $.ajax({
       type: "POST",
       url: "http://127.0.0.1:5000/echo",
-      data: JSON.stringify({"input":echoString}),
+      data: JSON.stringify({"input":message}),
       success: function(response){
         var outputDiv = $("#output").html(response.input);
         console.log(response);
@@ -19,15 +19,40 @@ $(document).ready(function(){
 
   $("#done_button").click(function() {
 
-    var echoString = $("#echo_input").val();
+    var message = $("#message_input").val();
+    var username = $("#username_input").val();
 
     $.ajax({
       type: "POST",
       url: "http://127.0.0.1:5000/messages",
-      data: JSON.stringify({"input":echoString}),
+      data: JSON.stringify({"username": username, "input":message}),
       success: function(response){
         var outputDiv = $("#output").html(response.input);
         console.log(response);
+      }
+    });
+
+  });
+
+  $("#get_button").click(function() {
+
+    var username = $("#username_input").val();
+
+    $.ajax({
+      type: "GET",
+      url: "http://127.0.0.1:5000/messages/"+username,
+      success: function(response){
+        console.log(response);
+
+        var outputDiv = $("#output")
+
+        outputDiv.empty();
+
+        var i;
+        for(i=0; i<response.results.length; i++){
+          outputDiv.append("<div>" + response.results[i].body + "<div>");
+        }
+
       }
     });
 
